667c6f5b8eaed756d6ecfc6f1488135c096d5a08
Kota Ishimoto <ishikota086@gmail.com>
2016-10-18 09:48:55 +0900

    Update setup.py to publish to pypi

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup
 
 setup(
     name = 'HoleCardHandicapper',
-    version = '0.0.1',
-    description = 'NeuralNet for poker hands evaluation ',
-    license = 'MIT',
+    version = '1.0.0',
     author = 'ishikota',
     author_email = 'ishikota086@gmail.com',
+    description = 'NeuralNet for poker hands evaluation ',
+    license = 'MIT',
+    keywords = 'python poker hand predict evaluate evaluator',
     url = 'https://github.com/ishikota/HoleCardHandicapper',
-    keywords = 'python poker hand predict evaluate',
     packages = ['holecardhandicapper', 'holecardhandicapper.model'],
     install_requires = ['Keras==1.0.4', 'h5py==2.6.0', 'PyPokerEngine==0.0.1'],
-    dependency_links = ['git+https://github.com/ishikota/PyPokerEngine.git@0.0.1#egg=PyPokerEngine-0.0.1']
-    )
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
