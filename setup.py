9f4f9bef82b8057a23a6d09eddac3b2d3b183140
Kota Ishimoto <ishikota086@gmail.com>
2016-12-08 01:13:00 +0900

    Bump up version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'HoleCardHandicapper',
-    version = '1.1.0',
+    version = '1.2.0',
     author = 'ishikota',
     author_email = 'ishikota086@gmail.com',
     description = 'NeuralNet for poker hands evaluation ',
@@ -11,7 +11,7 @@ setup(
     url = 'https://github.com/ishikota/HoleCardHandicapper',
     packages = ['holecardhandicapper', 'holecardhandicapper.model', ],
     package_data = {'holecardhandicapper' : ['model/weights/*.h5', 'model/data/preflop_winrate.csv']},
-    install_requires = ['Keras==1.0.4', 'h5py==2.6.0', 'PyPokerEngine==0.0.1'],
+    install_requires = ['Keras==1.0.4', 'h5py==2.6.0', 'PyPokerEngine'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
